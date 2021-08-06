b71117e870e966838416028c27ec9bd94a6458b1
Joel Posti <joel.posti@response200.pro>
2021-08-06 15:41:16 +0300

    Fixed deprecation warning: `In future versions of Node.js, fs.rmdir(path, { recursive: true }) will be removed. Use fs.rm(path, { recursive: true }) instead`

--- tests/features/scripts/hooks/scripts.ts
+++ tests/features/scripts/hooks/scripts.ts
@@ -4,6 +4,6 @@ import type { ScriptProps } from 'tests/features/scripts/helpers/scripts'
 
 After({ tags: '@scripts' }, async function (this: ScriptProps) {
   if (this?.testGitRepo?.length > 0) {
-    await fs.rmdir(this.testGitRepo, { recursive: true })
+    await fs.rm(this.testGitRepo, { recursive: true })
   }
 })
