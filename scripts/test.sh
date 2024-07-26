ada2008e3e793ebc23093b80fe21a761c483b88f
Joel Posti <joel.posti@response200.pro>
2024-07-26 11:59:18 +0300

    Removed `--publish-quiet` option from cucumber-js invocation in scripts/test.sh. The option is no longer needed. See https://github.com/cucumber/cucumber-js/blob/main/docs/deprecations.md#publishquiet for more information.

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,7 +6,7 @@ projectDir="$(cd "$(dirname "$0")" && git rev-parse --show-toplevel)"
 cd "$projectDir"
 
 ./node_modules/.bin/cucumber-js \
-  --format @cucumber/pretty-formatter --publish-quiet \
+  --format @cucumber/pretty-formatter \
   --parallel 2 \
   --require tests/babel-register.js \
   --require 'tests/**/hooks/*.ts' \
